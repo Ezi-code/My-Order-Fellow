8614f9c5f94f355072d56c51dc7609b095f8a319
ezi-code <emailyendau2000@hotmail.com>
2026-01-19 15:43:27 +0000

    created user authentication and added swagger docs

---
+++ users/managers.py
@@ -0,0 +1,33 @@
+"""user model manager."""
+
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """custom user manager."""
+
+    def create_user(self, email, username, password=None, **extra_fields):
+        """create and return a user with an email and password."""
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        """create and return a superuser with an email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, username, password, **extra_fields)
+
+    def create(self, email, username, password, **kwargs):
+        """create a user instance without saving to the database."""
+        return self.create_user(email, username, password, **kwargs)
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+"""users urls file."""
+
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("users/auth/login/", views.LoginView.as_view(), name="login"),
+    path("users/auth/logout/", views.LogoutView.as_view(), name="logout"),
+    path("users/auth/register/", views.RegisterView.as_view(), name="register"),
+]
